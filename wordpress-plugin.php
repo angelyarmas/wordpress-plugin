9ce9265b3170cdbc7343207dd696296d496e798c
Ángel Yarmas <angelyarmasmosquera@gmail.com>
2021-10-20 11:45:12 -0500

    first commit

---
+++ wordpress-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wordpress plugin
+ * Plugin URI:      https://github.com/angelyarmas/wordpress-plugin
+ * Description:     Base scaffold for WordPress plugins
+ * Author:          Ángel Yarmas
+ * Author URI:      https://github.com/angelyarmas
+ * Text Domain:     wordpress-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wordpress_Plugin
+ */
+
+// Your code starts here.
